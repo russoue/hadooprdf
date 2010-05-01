86d6109a4dac31ab81a851bdaf22de2cd48bef22
sharathjagannath@gmail.com <sharathjagannath@gmail.com@b85f50e6-92fd-b578-cf02-4bba7a10dc19>
2010-05-01 03:23:17 +0000

    Commiting intial version of Query Parser

---
+++ hadoopRdfQueryParser/src/edu/utdallas/hadooprdf/main/NotBasicElementException.java
@@ -0,0 +1,11 @@
+package edu.utdallas.hadooprdf.main;
+
+public class NotBasicElementException extends Exception {
+	public NotBasicElementException () {
+		super();
+	}
+	
+	public NotBasicElementException (String msg) {
+		super (msg);
+	}
+}
---
+++ hadoopRdfQueryParser/src/edu/utdallas/hadooprdf/main/UnhandledElementException.java
@@ -0,0 +1,12 @@
+package edu.utdallas.hadooprdf.main;
+
+public class UnhandledElementException extends Exception {
+
+	public UnhandledElementException () {
+		super();
+	}
+	
+	public UnhandledElementException (String msg) {
+		super (msg);
+	}
+}
