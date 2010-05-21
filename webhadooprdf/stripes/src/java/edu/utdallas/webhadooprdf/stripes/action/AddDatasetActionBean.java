73233df3440aebedbfcef3b49be2d7460b780b2c
asif.d2d3 <asif.d2d3@b85f50e6-92fd-b578-cf02-4bba7a10dc19>
2010-05-21 08:58:21 +0000

    Added Add datasets bean - not implemented

---
+++ webhadooprdf/stripes/src/java/edu/utdallas/webhadooprdf/stripes/action/AddDatasetActionBean.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.utdallas.webhadooprdf.stripes.action;
+
+/**
+ *
+ * @author Asif Mohammed
+ */
+public class AddDatasetActionBean extends BaseActionBean{
+    private  static final String view="/WEB-INF/jsp/add_dataset.jsp";
+}
