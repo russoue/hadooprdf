a3104aa88018a330cf90e896cf534e9e7a96df7c
russoue <russoue@gmail.com>
2010-09-17 01:31:14 +0000

    Added DictionaryPrefixTree. Finished logic to add new string and generate id based on reducer. Retrieval is not done yet.

---
+++ HadoopRDF/test/edu/utdallas/hadooprdf/data/preprocessing/dictionary/DictionaryCreatorTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package edu.utdallas.hadooprdf.data.preprocessing.dictionary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Mohammad Farhan Husain
+ *
+ */
+public class DictionaryCreatorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link edu.utdallas.hadooprdf.data.preprocessing.dictionary.DictionaryCreator#createDictionary()}.
+	 */
+	@Test
+	public void testCreateDictionary() {
+		fail("Not yet implemented");
+	}
+
+}
